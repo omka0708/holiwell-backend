4c55bd4b615bb58f84376272ac8cebbcd5bebc7a
omka <mcscare12@gmail.com>
2024-06-23 21:39:40 +0300

    fix delete lesson

--- app/lesson/models.py
+++ app/lesson/models.py
@@ -19,20 +19,20 @@ class Lesson(Base):
     course_id = Column(Integer, ForeignKey("course.id"))
     course = relationship("Course", back_populates="lessons", lazy='selectin')
 
-    planned_lessons = relationship("PlannedLesson", back_populates="lesson")
+    planned_lessons = relationship("PlannedLesson", back_populates="lesson", cascade="all, delete")
 
 
 class LinkBeforeLesson(Base):
     __tablename__ = "link_before_lesson"
     id = Column(Integer, primary_key=True, autoincrement=True)
 
-    lesson_id = Column(Integer, ForeignKey("lesson.id"))
-    linked_lesson_id = Column(Integer, ForeignKey("lesson.id"))
+    lesson_id = Column(Integer, ForeignKey("lesson.id", ondelete="cascade"))
+    linked_lesson_id = Column(Integer, ForeignKey("lesson.id", ondelete="cascade"))
 
 
 class LinkAfterLesson(Base):
     __tablename__ = "link_after_lesson"
     id = Column(Integer, primary_key=True, autoincrement=True)
 
-    lesson_id = Column(Integer, ForeignKey("lesson.id"))
-    linked_lesson_id = Column(Integer, ForeignKey("lesson.id"))
+    lesson_id = Column(Integer, ForeignKey("lesson.id", ondelete="cascade"))
+    linked_lesson_id = Column(Integer, ForeignKey("lesson.id", ondelete="cascade"))
